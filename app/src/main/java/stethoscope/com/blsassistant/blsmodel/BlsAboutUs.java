ed29c4776ee9b02f58d114a1d5cb9b9108e6abc1
sevenlol <sevenlol1007@gmail.com>
2015-01-10 18:35:14 +0800

    Fb, website, g+ link button function added.

--- app/src/main/java/stethoscope/com/blsassistant/blsmodel/BlsAboutUs.java
+++ app/src/main/java/stethoscope/com/blsassistant/blsmodel/BlsAboutUs.java
@@ -1,9 +1,13 @@
 package stethoscope.com.blsassistant.blsmodel;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import java.io.IOException;
@@ -13,8 +17,20 @@ import java.io.InputStream;
  * Created by stephen on 2015/1/7.
  */
 public class BlsAboutUs implements BlsTemplate {
+    private static final String STETHOSCOPE_FACEBOOK_URL = "http://www.google.com";
+    private static final String STETHOSCOPE_WEBSITE_URL = "http://www.google.com";
+    private static final String STETHOSCOPE_GOOGLE_PLUS_URL = "http://www.google.com";
+
     @Override
     public void setView(View v, int index, Context ctx, Handler handler) {
+        //set up about us link buttons (facebook, google plus, website)
+        ImageButton mFbButton = (ImageButton) v.findViewById(ctx.getResources().getIdentifier("aboutus_fb", "id", ctx.getPackageName()));
+        ImageButton mWebsiteButton = (ImageButton) v.findViewById(ctx.getResources().getIdentifier("aboutus_website", "id", ctx.getPackageName()));
+        ImageButton mGooglePlusButton = (ImageButton) v.findViewById(ctx.getResources().getIdentifier("aboutus_google_plus", "id", ctx.getPackageName()));
+        mFbButton.setOnClickListener(new BrowserButtonListener(STETHOSCOPE_FACEBOOK_URL, ctx));
+        mWebsiteButton.setOnClickListener(new BrowserButtonListener(STETHOSCOPE_WEBSITE_URL, ctx));
+        mGooglePlusButton.setOnClickListener(new BrowserButtonListener(STETHOSCOPE_GOOGLE_PLUS_URL, ctx));
+
         try{
             ImageView mImage = (ImageView) v.findViewById(ctx.getResources().getIdentifier("aboutus_icon", "id", ctx.getPackageName()));
             InputStream ims = ctx.getAssets().open("stethoscope_logo.png");
@@ -45,5 +61,21 @@ public class BlsAboutUs implements BlsTemplate {
     public boolean contains(String searchStr) {
         //never get searched
         return false;
+    }
+
+    public static class BrowserButtonListener implements View.OnClickListener {
+        private Context ctx;
+        private String  url;
+
+        public BrowserButtonListener(String url, Context ctx) {
+            this.ctx = ctx;
+            this.url = url;
+        }
+
+        @Override
+        public void onClick(View v) {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            ctx.startActivity(browserIntent);
+        }
     }
 }
